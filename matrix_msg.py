47d47186e38a57920225d562d25f5f716243dab1
Adam Ranson <ranson.ad@gmail.com>
2023-04-02 08:00:41 +0200

    op of room msg

--- matrix_msg.py
+++ matrix_msg.py
@@ -15,7 +15,7 @@ class CustomMatrixHttpApi(MatrixHttpApi):
         content = self._send("GET", f"/rooms/{room_id}/members")
         return content
     
-def main(target_user,msg):
+def main(target_user,msg,group=''):
     # convert ubuntu username to element username
     target_user = lookup_user(target_user)
 
@@ -32,20 +32,27 @@ def main(target_user,msg):
     # Get a list of rooms the bot is in
     rooms = client.get_rooms()
 
-    # Check if there's already a private room with the target user
-    target_room = None
-    for room_id, room in rooms.items():
-        members = room.get_joined_members()
-        # make dict of all members
-        all_members = []
-        for iMem in range(len(members)):
-            all_members.append(members[iMem].user_id)
-        if target_user in all_members and len(members) == 2:
-            target_room = room    
-            # Send the message
-            target_room.send_text(msg)
-            break
-
+    if group == '':
+        # Check if there's already a private room with the target user
+        target_room = None
+        for room_id, room in rooms.items():
+            members = room.get_joined_members()
+            # make dict of all members
+            all_members = []
+            for iMem in range(len(members)):
+                all_members.append(members[iMem].user_id)
+            if target_user in all_members and len(members) == 2:
+                target_room = room    
+                # Send the message
+                target_room.send_text(msg)
+                break
+    else:
+        # try to find the group requested !byLPEQmNPNnuSYFYuP:matrix.org
+        for room_id, room in rooms.items():
+            display_name = room.display_name
+            if display_name == group:
+                room.send_text(msg)
+        
     # If no private room exists, create one and invite the target user
     # if not target_room:
     #     target_room = client.create_room(is_public=False, invitees=[target_user])
