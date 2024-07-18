4c046263f7b334128ffc3a2047f63c0f2656dbac
Adam Ranson <ranson.ad@gmail.com>
2024-07-18 08:40:46 +0200

    test

--- scatch.py
+++ scatch.py
@@ -1,3 +1,3 @@
 import deeplabcut
-
+a
 deeplabcut.analyze_videos('/data/common/dlc_models/eye/config.yaml', '/home/adamranson/dlc/test1/2023-05-15_06_ESMT134_eye1_left.avi', videotype='avi', shuffle=1, gputouse=0, save_as_csv=True, destfolder='/home/adamranson/dlc/test1/')
